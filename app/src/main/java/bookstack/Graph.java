960fc00db350b9370b2e4dc47210f345367ededf
Allan Cao <acao737@gmail.com>
2015-02-25 19:37:08 -0500

    Bluetooth works, need to change ID to actual component

---
+++ app/src/main/java/bookstack/Graph.java
@@ -0,0 +1,96 @@
+package bookstack;
+
+import android.app.Fragment;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.androidplot.xy.BoundaryMode;
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.PointLabelFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYPlot;
+import com.androidplot.xy.XYSeries;
+import com.androidplot.xy.XYStepMode;
+import bookstack.R;
+
+import java.text.DecimalFormat;
+import java.util.Arrays;
+
+public class Graph extends Fragment {
+
+    XYPlot plot;
+    private SimpleXYSeries mySeries;
+
+    public Graph() {
+        // Empty constructor required for fragment subclasses
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.graph, container, false);
+        plot = (XYPlot) rootView.findViewById(R.id.mySimpleXYPlot);
+
+//            plot.setBorderStyle(Plot.BorderStyle.NONE, null, null);
+//            plot.setPlotMargins(0, 0, 0, 0);
+//            plot.setPlotPadding(0, 0, 0, 0);
+//            plot.setGridPadding(0, 0, 0, 0);
+
+        Number[] days =   { 1  , 2   , 3   , 4   , 5   , 6   , 7 };
+        Number[] values = { 10, 30, 40, 60, 100, 95, 70};
+
+        XYSeries mySeries = new SimpleXYSeries(
+                Arrays.asList(days),
+                Arrays.asList(values),
+                "Series1");
+
+        LineAndPointFormatter format = new LineAndPointFormatter(
+                Color.rgb(200, 200, 200),                   // line color
+                Color.parseColor("#00000000"),                   // point color
+                Color.parseColor("#59000000"),          // fill color
+                new PointLabelFormatter(Color.TRANSPARENT));
+        format.getLinePaint().setStrokeWidth(5);
+        format.getVertexPaint().setStrokeWidth(10);
+
+//            LineAndPointFormatter format = new LineAndPointFormatter();
+//            format.setPointLabelFormatter(new PointLabelFormatter());
+//            format.configure(getActivity(), R.xml.line_point_formatter_with_plf1);
+        plot.addSeries(mySeries, format);
+
+//            plot.getGraphWidget().getBackgroundPaint().setColor(Color.WHITE);
+//            plot.getGraphWidget().getGridBackgroundPaint().setColor(Color.WHITE);
+
+        plot.getGraphWidget().getDomainOriginLabelPaint().setColor(Color.BLACK);
+        plot.getGraphWidget().getDomainOriginLinePaint().setColor(Color.BLACK);
+        plot.getGraphWidget().getRangeOriginLinePaint().setColor(Color.BLACK);
+
+        plot.getGraphWidget().getDomainLabelPaint().setColor(Color.parseColor("#E6E6E6"));
+        plot.getGraphWidget().getRangeLabelPaint().setColor(Color.parseColor("#E6E6E6"));
+
+        // Domain
+        plot.setDomainStep(XYStepMode.INCREMENT_BY_VAL, days.length);
+        plot.setDomainValueFormat(new DecimalFormat("0"));
+        plot.setDomainStepValue(1);
+
+        //Range
+        plot.setRangeBoundaries(0, 120, BoundaryMode.FIXED);
+        plot.setRangeStepValue(5);
+        //mySimpleXYPlot.setRangeStep(XYStepMode.SUBDIVIDE, values.length);
+        plot.setRangeValueFormat(new DecimalFormat("0"));
+
+        //Remove legend
+        plot.getLayoutManager().remove(plot.getLegendWidget());
+//            plot.getLayoutManager().remove(plot.getDomainLabelWidget());
+//            plot.getLayoutManager().remove(plot.getRangeLabelWidget());
+        plot.getLayoutManager().remove(plot.getTitleWidget());
+        plot.getLayoutManager().remove(plot.getLegendWidget());
+
+        plot.redraw();
+        getActivity().setTitle("Graph");
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/bookstack/RecommendationsFragment.java
@@ -0,0 +1,36 @@
+package bookstack;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import bookstack.R;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class RecommendationsFragment extends Fragment {
+    public static final String ARG_PLANET_NUMBER = "planet_number";
+
+    public RecommendationsFragment() {
+        // Empty constructor required for fragment subclasses
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.graph, container, false);
+        return rootView;
+    }
+
+    public String sign() throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException {
+        SignedRequestsHelper signedRequestsHelper = new SignedRequestsHelper();
+        String signed = signedRequestsHelper.sign(UrlParameterHandler.getInstance().buildMapForItemSearch());
+
+        return signed;
+    }
+}
